c01ad9477665b3ada04e9312e114d305fedd8db8
mauricioortiz1991 <mauricioortiz1991@gmail.com>
2025-05-21 20:22:22 -0300

    clase 2 - 20250430

---
+++ 20250430/estructuras-control.py
@@ -0,0 +1,45 @@
+# condicion = True
+# condicion_2 = True
+# condicion_3 = True
+
+# # Condicional simple
+# if condicion:
+#     print('Se cumple condicion')
+
+# # Condicional alternativo
+# if condicion:
+#     print('Se cumple condicion')
+# else:
+#     print('No se cumple condicion')
+
+# # Condicional multiple
+# if condicion:
+#     print('Se cumple condicion')
+# elif(condicion_2):
+#     print('Se cumple condicion_2')
+# elif(condicion_3):
+#     print('Se cumple condicion_3')
+
+# # Condicional multiple alternativo
+# if condicion:
+#     print('Se cumple condicion')
+# elif(condicion_2):
+#     print('Se cumple condicion_2')
+# else:
+#     print('No se cumple ninguna condicion')
+
+# Iterativos
+# variables = [11,22,33,44,55]
+# # for x in variables:
+# #     print('Estoy en el elemento', x)
+# for x in range(100):
+#     print('Estoy en el elemento', x)
+
+# Cortamos el proceso con CTRL + C
+# while True:
+#     print('Sigo ejecutando...')
+
+contador = 0
+while (contador != 5):
+    print('contador vale:', contador)
+    contador = contador + 1
---
+++ 20250430/variables-tipo-dato.py
@@ -0,0 +1,67 @@
+# ## variables enteras
+# int_1 = 5
+# int_2 = 2
+
+# # suma
+# suma = int_1 + int_2
+# print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+# # resta
+# resta = int_1 - int_2
+# print(f"La resta entre {int_1} y {int_2} es: {resta}")
+
+# # multiplicacion
+# multiplicacion = int_1 * int_2
+# print(f"La multiplicacion entre {int_1} y {int_2} es: {multiplicacion}")
+
+# # division
+# division = int_1 / int_2
+# print(f"La division entre {int_1} y {int_2} es: {division}")
+
+# # potencia
+# potencia = int_1 ** int_2
+# print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+# ## variables string
+# str_1 = "Info"
+# str_2 = "rmatorio"
+
+# ## operadores
+# #suma
+# suma = str_1 + str_2
+# print(f"La suma entre {str_1} y {str_2} es: {suma}")
+
+# ## variables listas
+# list_1 = [1, 2, 3]
+# list_2 = [4, 5, 6]
+
+# ## operadores
+# #suma
+# suma = list_1 + list_2
+# print(f"La suma entre {list_1} y {list_2} es: {suma}")
+
+# #resta no se puede restar
+# resta = list_1 - list_2
+# print(f"La suma entre {list_1} y {list_2} es: {resta}")
+
+# lista = [93,12389,203,21982,38787,123,1,23,4]
+# # utilizando .sort()
+# lista.sort()
+
+
+# print(lista)
+
+# lista = [10,20,30,40,50]
+# print(lista[-1])
+
+# lista = ['gerardo', 'andres', 'ainara']
+# lista.sort()
+# print(lista)
+# lista.sort(reverse=True)
+# print(lista)
+
+# tupla = ('Gerardo', 'Andres', 'Ainara', 3, 1)
+# print(tupla[0])
+
+diccionario = {45302258: 'Pablo Perez', 45825693: 'Pablo Granados'}
+print(diccionario[45825693])
